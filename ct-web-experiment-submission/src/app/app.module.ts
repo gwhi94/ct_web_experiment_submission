47fd189a716ba075df625d0dfe16e8058d3c54ce
George Whiting <gwhi94@gmail.com>
2023-05-13 20:37:20 +0100

    angular - firebase

---
+++ ct-web-experiment-submission/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ct-web-experiment-submission/src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+
+  
+  firebase: {
+    apiKey: "AIzaSyD3asyNfdrhBLliK7hcsu0ShNhI09URDQQ",
+    authDomain: "ct-web-experiment-submission.firebaseapp.com",
+    databaseURL: "https://ct-web-experiment-submission-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ct-web-experiment-submission",
+    storageBucket: "ct-web-experiment-submission.appspot.com",
+    messagingSenderId: "131999637762",
+    appId: "1:131999637762:web:88dc43c921cc2cbe42a21b",
+    measurementId: "G-2LGJQ0YRL8"
+  }
+};
+
+
